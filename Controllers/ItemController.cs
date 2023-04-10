f86fe9cfb7a4877439737c8b065e148361c5b99e
DylanYoan <dylanyoanmc@gmail.com>
2023-04-10 01:21:57 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using SubIn.Models;
+
+namespace SubIn.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: ItemController
+        public ActionResult Index()
+        {
+            return View(new Item().GetItem());
+        }
+
+        // GET: ItemController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ItemController/Create
+        public ActionResult Create()
+        {
+            return View(new Item());
+        }
+
+        // POST: ItemController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormCollection collection, Item itemcito)
+        {
+            try
+            {
+                itemcito.AddItem(itemcito);
+                return View(itemcito);
+            }
+            catch (Exception e)
+            {
+                string msg = e.Message;
+                return View(itemcito);
+            }
+        }
+
+        // GET: ItemController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(new Item().ObtenerItemById(id));
+        }
+
+        // POST: ItemController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, FormCollection collection, Item itemcito)
+        {
+            try
+            {
+                itemcito.UpdateItem(itemcito);
+                return View("Index", new Item().GetItem());
+            }
+            catch (Exception e)
+            {
+                string msg = e.Message;
+                return View();
+            }
+        }
+
+        // GET: ItemController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(new Item().ObtenerItemById(id));
+        }
+
+        // POST: ItemController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection collection, Item itemcito)
+        {
+            try
+            {
+                itemcito.DeleteItem(itemcito); return View("Index");
+            }
+            catch (Exception e)
+            {
+                string msg = e.Message;
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Models/MyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace SubIn.Models
+{
+
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext() : base(GetConnection(), true)
+            {
+            }
+
+            private static NpgsqlConnection GetConnection()
+            {
+                var connString = "Host=localhost;Username=postgres;Password=bd;Database=subin";
+                return new NpgsqlConnection(connString);
+            }
+            public DbSet<Usuario> Usuario { get; set; }
+        }
+
+        
+    
+
+
+    
+  
+}
